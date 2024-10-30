863502cb434f9ffce5f86351e42f7708c6bce655
lockdoor <lockdoor@gmail.com>
2024-10-30 12:40:18 +0700

    before add next@canary

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,9 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { CardSkeleton } from '@/app/ui/skeletons';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +17,11 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const { 
+    totalPaidInvoices, 
+    totalPendingInvoices, 
+    numberOfInvoices, 
+    numberOfCustomers } = await fetchCardData()
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
@@ -26,6 +34,16 @@ export default async function CardWrapper() {
         value={numberOfCustomers}
         type="customers"
       /> */}
+
+      
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
     </>
   );
 }
